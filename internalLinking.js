816e3e166767a2df8a8a25fd37f339af0557929f
pinterestseo <rispoker31@gmail.com>
2024-12-04 17:02:36 +0700

    updatepost2

--- internalLinking.js
+++ internalLinking.js
@@ -6,59 +6,73 @@ const PAGES_DIR = './src/pages/question';
 
 const questions = [
   {
-    id: 1733223173892,
-    title: "Poze cu mesaje de la mulți ani:  Îmbrățișări calde, sufletești",
-    description: "Comprehensive guide about Poze cu mesaje de la mulți ani:  Îmbrățișări calde, sufletești",
-    slug: "poze-cu-mesaje-de-la-multi-ani",
+    id: 1733305018735,
+    title: "Urări Calde: Mesaje de la mulți ani 18 ani scurte",
+    description: "Comprehensive guide about Urări Calde: Mesaje de la mulți ani 18 ani scurte",
+    slug: "mesaje-de-la-multi-ani-18-ani-scurte",
     category: "Mesaje-La-Multi-Ani"
   },
   {
-    id: 1733223146968,
-    title: "Îmbrățișări Calde: Mesaje de la Mulți Ani pentru Mama",
-    description: "Comprehensive guide about Îmbrățișări Calde: Mesaje de la Mulți Ani pentru Mama",
-    slug: "mesaje-de-la-multi-ani-pentru-mama",
+    id: 1733305000669,
+    title: "Urare Caldă: Mesaje la Mulți Ani Prietena",
+    description: "Comprehensive guide about Urare Caldă: Mesaje la Mulți Ani Prietena",
+    slug: "mesaje-la-multi-ani-prietena",
     category: "Mesaje-La-Multi-Ani"
   },
   {
-    id: 1733223113040,
-    title: "Urare Caldă: Mesaje de la mulți ani frumoase și scurte",
-    description: "Comprehensive guide about Urare Caldă: Mesaje de la mulți ani frumoase și scurte",
-    slug: "mesaje-de-la-multi-ani-frumoase-si-scurte",
+    id: 1733304979643,
+    title: "Îmbrățișări Calde: Mesaje Frumoase de La Mulți Ani",
+    description: "Comprehensive guide about Îmbrățișări Calde: Mesaje Frumoase de La Mulți Ani",
+    slug: "mesaje-frumoase-de-la-multi-ani",
     category: "Mesaje-La-Multi-Ani"
   },
   {
-    id: 1733223093474,
-    title: "Îmbrățișări Calde: Mesaje la Mulți Ani pline de Dragoste",
-    description: "Comprehensive guide about Îmbrățișări Calde: Mesaje la Mulți Ani pline de Dragoste",
-    slug: "mesaje-la-multi-ani",
+    id: 1733304958439,
+    title: "Îmbrățișări Calde: Mesaje de la mulți ani pentru iubit",
+    description: "Comprehensive guide about Îmbrățișări Calde: Mesaje de la mulți ani pentru iubit",
+    slug: "mesaje-de-la-multi-ani-pentru-iubit",
     category: "Mesaje-La-Multi-Ani"
   },
   {
-    id: 1733223068739,
-    title: "Îmbrățișări Calde: La Mulți Ani Mesaje pline de Dragoste",
-    description: "Comprehensive guide about Îmbrățișări Calde: La Mulți Ani Mesaje pline de Dragoste",
-    slug: "la-multi-ani-mesaje",
+    id: 1733304936173,
+    title: "Îmbrățișări Calde: Mesaje cu La Mulți Ani",
+    description: "Comprehensive guide about Îmbrățișări Calde: Mesaje cu La Mulți Ani",
+    slug: "mesaje-cu-la-multi-ani",
     category: "Mesaje-La-Multi-Ani"
   },
   {
-    id: 1733223044687,
-    title: "Îmbrățișări Calde: Cele Mai Frumoase Mesaje de La Mulți Ani",
-    description: "Comprehensive guide about Îmbrățișări Calde: Cele Mai Frumoase Mesaje de La Mulți Ani",
-    slug: "cele-mai-frumoase-mesaje-de-la-multi-ani",
+    id: 1733304915766,
+    title: "Binecuvântate Mesaje Creștine de la Mulți Ani pentru Fiica Mea",
+    description: "Comprehensive guide about Binecuvântate Mesaje Creștine de la Mulți Ani pentru Fiica Mea",
+    slug: "mesaje-crestine-de-la-multi-ani-pentru-fiica-mea",
     category: "Mesaje-La-Multi-Ani"
   },
   {
-    id: 1733223024780,
-    title: "Îmbrățișări Calde: Mesaje de La Mulți Ani",
-    description: "Comprehensive guide about Îmbrățișări Calde: Mesaje de La Mulți Ani",
-    slug: "mesaje-de-la-multi-ani",
+    id: 1733304895693,
+    title: "Urare Caldă: Mesaje de la Mulți Ani pentru Copii",
+    description: "Comprehensive guide about Urare Caldă: Mesaje de la Mulți Ani pentru Copii",
+    slug: "mesaje-de-la-multi-ani-pentru-copii",
     category: "Mesaje-La-Multi-Ani"
   },
   {
-    id: 1733223001059,
-    title: "Îmbrățișări Calde: Mesaje la Mulți Ani pline de Dragoste",
-    description: "Comprehensive guide about Îmbrățișări Calde: Mesaje la Mulți Ani pline de Dragoste",
-    slug: "mesaje-la-multi-ani",
+    id: 1733304875030,
+    title: "Îmbrățișare Caldă: Mesaje de la mulți ani pentru soț",
+    description: "Comprehensive guide about Îmbrățișare Caldă: Mesaje de la mulți ani pentru soț",
+    slug: "mesaje-de-la-multi-ani-pentru-sot",
+    category: "Mesaje-La-Multi-Ani"
+  },
+  {
+    id: 1733304855741,
+    title: "Urare Caldă: Mesaje de la Mulți Ani pentru Băiat",
+    description: "Comprehensive guide about Urare Caldă: Mesaje de la Mulți Ani pentru Băiat",
+    slug: "mesaje-de-la-multi-ani-pentru-baiat",
+    category: "Mesaje-La-Multi-Ani"
+  },
+  {
+    id: 1733304835637,
+    title: "Îmbrățișare caldă: Mesaje de la mulți ani pentru fiica mea tpu",
+    description: "Comprehensive guide about Îmbrățișare caldă: Mesaje de la mulți ani pentru fiica mea tpu",
+    slug: "mesaje-de-la-multi-ani-pentru-fiica-mea-tpu",
     category: "Mesaje-La-Multi-Ani"
   }
   // ... data dari questions.ts
